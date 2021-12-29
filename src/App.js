fa8f66d73e6f0eaf19e5922b9329ed5049ecfd03
MaksVibe <parunovmaksym@gmail.com>
2021-12-29 15:49:47 +0200

    React App + Redux + Toolkit + Router + EsLint

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
