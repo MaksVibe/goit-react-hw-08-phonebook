58bf4cb2b968d8c1f7568233c03f671579dccfa7
MaksVibe <parunovmaksym@gmail.com>
2022-01-13 12:04:37 +0200

    Update Api.js

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,17 +2,10 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://connections-api.herokuapp.com";
 
-const getData = (endpoint) => {
-  return axios.get(endpoint);
-};
+const getData = (endpoint) => axios.get(endpoint);
 
-const saveItem = (endpoint, item) => {
-  return axios.post(endpoint, item);
-};
+const saveItem = (endpoint, item) => axios.post(endpoint, item);
 
-const deleteItem = (endpoint, id) => {
-  console.log(`id`, id);
-  return axios.delete(`${endpoint}/${id}`);
-};
+const deleteItem = (endpoint, id) => axios.delete(`${endpoint}/${id}`);
 
 export { getData, saveItem, deleteItem };
